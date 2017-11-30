199df83793b492947682c07dcbaf0add543f042e
Riturajcse <riturajcse1994@gmail.com>
2017-11-30 18:06:27 +0530

    adding files

---
+++ installNodeApp.sh
@@ -0,0 +1,7 @@
+# script for installing and starting a sample node web application on port 3000
+sudo apt-get install git
+sudo apt install nodejs-legacy -y
+sudo apt install npm -y
+git clone https://github.com/Riturajcse/sampleNodeApp.git
+cd sampleNodeApp
+npm start &
